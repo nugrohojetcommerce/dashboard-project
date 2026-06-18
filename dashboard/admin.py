e0a329a56e1e1af74d1ee99a2d1793810d982b91
nugrohojetcommerce <nugroho.raharjo@jetcommerce.co.id>
2026-06-18 18:08:27 +0700

    initial dashboard view

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,5 +1,92 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
 from .models import Brand, UserBrand
 
-admin.site.register(Brand)
-admin.site.register(UserBrand)
+
+class UserBrandForm(forms.ModelForm):
+    assigned_brands = forms.ModelMultipleChoiceField(
+        queryset=Brand.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+        label="Assigned Brands",
+    )
+
+    class Meta:
+        model = User
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if self.instance and self.instance.pk:
+            self.fields["assigned_brands"].initial = (
+                UserBrand.objects.filter(user=self.instance)
+                .values_list("brand_id", flat=True)
+            )
+
+
+admin.site.unregister(User)
+
+
+@admin.register(User)
+class CustomUserAdmin(BaseUserAdmin):
+    form = UserBrandForm
+
+    fieldsets = BaseUserAdmin.fieldsets + (
+        (
+            "Brand Access Control",
+            {
+                "fields": (
+                    "assigned_brands",
+                )
+            },
+        ),
+    )
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+
+        selected_brands = form.cleaned_data.get(
+            "assigned_brands",
+            Brand.objects.none(),
+        )
+
+        UserBrand.objects.filter(user=obj).delete()
+
+        UserBrand.objects.bulk_create(
+            [
+                UserBrand(
+                    user=obj,
+                    brand=brand,
+                )
+                for brand in selected_brands
+            ]
+        )
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+
+        if request.user.is_superuser:
+            return qs
+
+        return qs.filter(id=request.user.id)
+
+    def has_change_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+
+        return obj is not None and obj.id == request.user.id
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+    def has_module_permission(self, request):
+        return request.user.is_superuser
--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import dashboard_view, api_gmv, api_brand_performance, brand_performance_view
+from .views import *
 
 urlpatterns = [
-    path('', dashboard_view),
+    # path('', dashboard_view)
+    path('',brand_performance_view),
     path('api/gmv', api_gmv),
     path('api/brand_performance',api_brand_performance),
-    path('brand_performance/', brand_performance_view)
+    path('brand_performance/',brand_performance, name="brand_performance")
 ]

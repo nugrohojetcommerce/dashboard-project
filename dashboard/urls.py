335c96c9bcac41f7f16a7141b580ffad797396b0
nugrohojetcommerce <nugroho.raharjo@jetcommerce.co.id>
2026-06-22 13:57:13 +0700

    Revert "Revert "change the backend load into frontend load""

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import *
+from .views import brand_performance, brand_performance_api
 
 urlpatterns = [
     # path('', dashboard_view)
-    path('',brand_performance_view),
-    path('api/gmv', api_gmv),
-    path('api/brand_performance',api_brand_performance),
-    path('brand_performance/',brand_performance, name="brand_performance")
+    # path('',brand_performance_view),
+    # path('api/gmv', api_gmv),
+    # path('api/brand_performance',api_brand_performance),
+    path('',brand_performance, name="brand_performance"),
+    path(
+        "api/brand-performance/",
+        brand_performance_api,
+        name="brand_performance_api",
+    ),
 ]
